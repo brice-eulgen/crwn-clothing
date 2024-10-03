b40b4fd4598886911893242ba5d9028234e89b55
brice-eulgen <mastaflex65@gmail.com>
2024-10-03 09:53:42 +0100

    update code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+	return (
+		<div>
+			<h1>HATS PAGE</h1>
+		</div>
+	);
+};
 
 function App() {
 	return (
 		<div className="App">
-			<HomePage/>
+			<Routes>
+				<Route index element={<HomePage />} />
+				<Route path="/hats" element={<HatsPage />} />
+			</Routes>
 		</div>
 	);
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,7 @@ import Directory from "../../components/directory/directory.components";
 const HomePage = () => {
 	return (
 		<div className="homepage">
-			<Directory />
+			<Directory/>
 		</div>
 	);
 };
