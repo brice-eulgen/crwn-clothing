2635c5e7a845ebc039b9ea8bc36fea62d6818e1a
brice-eulgen <mastaflex65@gmail.com>
2024-10-09 12:54:51 +0100

    updating shop page, collection preview and create collection item page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-	return (
-		<div>
-			<h1>HATS PAGE</h1>
-		</div>
-	);
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route index element={<HomePage />} />
-				<Route path="/hats" element={<HatsPage />} />
+				<Route path="/shop" element={<ShopPage />} />
 			</Routes>
 		</div>
 	);
--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -1,47 +1,22 @@
 import React from "react";
 import "./directory.styles.scss";
 import MenuItem from "../menu-item/menu-item.components";
+import SECTIONS_DATA from "../../data/section.data";
 
 class Directory extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			sections: [
-				{
-					title: "Hats",
-					id: 1,
-					imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-				},
-				{
-					title: "Jackets",
-					id: 2,
-					imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-				},
-				{
-					title: "Sneakers",
-					id: 3,
-					imageUrl: "https://i.ibb.co/0jqHpnp/jackets.png",
-				},
-				{
-					title: "Womens",
-					id: 4,
-					imageUrl: "https://i.ibb.co/GCCdy8t/jackets.png",
-					size: "large",
-				},
-				{
-					title: "Mens",
-					id: 5,
-					imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-					size: "large",
-				},
-			],
+			sections: SECTIONS_DATA,
 		};
 	}
 	render() {
 		return (
 			<div className="directory-menu">
-				{this.state.sections.map(({ title, id, imageUrl,size }) => {
-					return <MenuItem title={title} imageUrl={imageUrl} key={id} size={size} />;
+				{this.state.sections.map(({ title, id, imageUrl, size }) => {
+					return (
+						<MenuItem title={title} imageUrl={imageUrl} key={id} size={size} />
+					);
 				})}
 			</div>
 		);
